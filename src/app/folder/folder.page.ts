a1f8b0a192cf7e0113839166787a068bbaab0eb4
Bedelo <s_ceb@hotmail.com>
2021-02-25 18:53:44 +0100

    debut du classeur

--- src/app/folder/folder.page.ts
+++ src/app/folder/folder.page.ts
@@ -8,11 +8,61 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class FolderPage implements OnInit {
   public folder: string;
+  public team;
+  public psgTeam = [
+    {
+      nom: 'mbappe',
+      numero: '7',
+      img: '/assets/profil_mbappe.png',
+      detail: '...',
+    },
+    {
+      nom: 'neymar',
+      numero: '10',
+      img: '/assets/profil_neynmar.png',
+      detail: '...',
+    },
+    {
+      nom: 'navas',
+      numero: '1',
+      img: '/assets/profile_navas.png',
+      detail: '...',
+    },
+  ];
+  public realTeam = [
+    {
+      nom: 'mbappe',
+      numero: '7',
+      img: '/assets/profil_mbappe.png',
+      detail: '...',
+    },
+  ];
+  public omTeam = [
+    {
+      nom: 'mbappe',
+      numero: '7',
+      img: '/assets/profil_mbappe.png',
+      detail: '...',
+    },
+  ];
 
-  constructor(private activatedRoute: ActivatedRoute) { }
+  constructor(private activatedRoute: ActivatedRoute) {}
 
   ngOnInit() {
     this.folder = this.activatedRoute.snapshot.paramMap.get('id');
+    switch (this.folder) {
+      case 'PSG':
+        this.team = this.psgTeam;
+        console.log(this.team);
+        break;
+      case 'REAL':
+        this.team = this.realTeam;
+        console.log(this.team);
+        break;
+      case 'OM':
+        this.team = this.omTeam;
+        console.log(this.team);
+        break;
+    }
   }
-
 }
